04ae584b1a09164ad7e1183b269e8f3d390a34c2
Alex Handby <alex.handby@bbc.co.uk>
2019-08-12 17:21:46 +0100

    added a test of go regex

--- src/regex-compose_test.go
+++ src/regex-compose_test.go
@@ -1,6 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"testing"
 	. "regexp"
-	. "strings"
 )
+
+func Test_findStringIndex(t *testing.T){
+
+	fileAsSection := createSectionFromString(codeSection)
+
+	fmt.Println(fileAsSection.toString())
+
+	exampleBuild, err := fileAsSection.find(MustCompile("stage\\(\\'Example Build\\'\\)"))
+
+	if err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	fmt.Println(exampleBuild.toString())
+
+}
