9fe165101d669b6e2076f1ca1a3d5ab1135692bc
Alex Handby <alex.handby@bbc.co.uk>
2019-08-13 13:31:19 +0100

    found QuoteMeta which does a fantastic job of escaping regex reserved characters

--- src/regex-compose_test.go
+++ src/regex-compose_test.go
@@ -10,10 +10,15 @@ func Test_findStringIndex(t *testing.T){
 
 	fileAsSection := createSectionFromString(codeSection)
 
-	fmt.Println(fileAsSection.toString())
+	//fmt.Println(fileAsSection.toString())
 
-	exampleBuild, err := fileAsSection.find(MustCompile("stage\\(\\'Example Build\\'\\)"))
+	//another := "foo\nbar\n"
 
+	//fmt.Print(another)
+
+	exampleBuild, err := fileAsSection.find(MustCompile(QuoteMeta(`stage('Example Build') {`)))
+
+	
 	if err != nil {
 		t.Errorf(err.Error())
 		return
