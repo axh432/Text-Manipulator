87d79c4a9bd835994559e7317e3a7e357b32ffd5
Alex Handby <alex.handby@bbc.co.uk>
2019-08-22 13:43:49 +0100

    created splitKeepDelimiter and started parsing

--- src/regex-compose.go
+++ src/regex-compose.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+
+	. "regexp"
+)
+
+func splitKeepDelimiter(pattern *Regexp, toBeSplit string) []string {
+
+	splitStrings := []string{}
+
+	matches := pattern.FindAllStringIndex(toBeSplit, -1)
+
+	if matches != nil {
+
+		for i, match := range matches {
+
+			matchStart := match[0]
+			matchEnd := match[1]
+
+			if i == 0 {
+
+				if matchStart > 0 {
+					splitStrings = append(splitStrings, toBeSplit[:matchStart])
+				}
+
+			}
+
+			if i > 0 {
+
+				previousMatch := matches[i-1]
+				previousMatchEnd := previousMatch[1]
+
+				if matchStart - previousMatchEnd > 0 {
+					splitStrings = append(splitStrings, toBeSplit[previousMatchEnd:matchStart])
+				}
+
+			}
+
+			splitStrings = append(splitStrings, toBeSplit[matchStart:matchEnd])
+
+			if i == len(matches) - 1 {
+				splitStrings = append(splitStrings, toBeSplit[matchEnd:])
+			}
+
+		}
+
+	}
+
+
+	return splitStrings
+}
--- src/regex-compose_test.go
+++ src/regex-compose_test.go
@@ -2,23 +2,107 @@ package main
 
 import (
 	"fmt"
-	"testing"
 	. "regexp"
+	"strings"
+	"testing"
 )
+
+var codeReference string = `
+import java.util.Scanner;
+
+class CheckEvenOdd
+{
+  public static void main(String args[])
+  {
+    int num;
+    System.out.println("Enter an Integer number:");
+
+    //The input provided by user is stored in num
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
+
+    /* If number is divisible by 2 then it's an even number
+     * else odd number*/
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+  }
+}
+`
+
+type Keyword struct {}
+type Name struct {}
+type Symbol struct {}
+
+type Package struct {
+	Name string
+	subPackage *Package
+}
+
+type ImportStatement struct {
+	ImportKeyWord string
+	Package *Package
+}
+
+func parseImportStatement(begin int, splitStrings []string) (int, ImportStatement){
+
+	for i := begin; i < len(splitStrings); i++ {
+
+		token := splitStrings[i]
+
+ 		if strings.Contains(token, "."){
+
+		}else if strings.Contains(token, ";"){
+
+		}
+	}
+
+}
+
+func Test_parse(t *testing.T){
+
+	re := MustCompile(`[[:punct:]]|[[:space:]]`)
+
+	splitStrings := splitKeepDelimiter(re, codeReference)
+
+	for i := 0; i < len(splitStrings); i++ {
+
+		token := splitStrings[i]
+
+		if strings.Contains(token, "import"){
+		}
+
+	}
+
+}
+
+func Test_splitKeepDelimiter(t *testing.T){
+
+	re := MustCompile(`[[:punct:]]|[[:space:]]`)
+
+	splitStrings := splitKeepDelimiter(re, codeReference)
+
+	for _, token := range splitStrings {
+		fmt.Println(token)
+	}
+
+
+}
 
 func Test_findStringIndex(t *testing.T){
 
 	fileAsSection := createSectionFromString(codeSection)
 
-	//fmt.Println(fileAsSection.toString())
+	exampleBuild, err := fileAsSection.find(MustCompile(QuoteMeta(`        stage('Example Build') {
+            agent { docker 'maven:3-alpine' } 
+            steps {
+                echo 'Hello, Maven'
+                sh 'mvn --version'
+            }
+        }`)))
 
-	//another := "foo\nbar\n"
 
-	//fmt.Print(another)
-
-	exampleBuild, err := fileAsSection.find(MustCompile(QuoteMeta(`stage('Example Build') {`)))
-
-	
 	if err != nil {
 		t.Errorf(err.Error())
 		return
